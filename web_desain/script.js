8ad5655cef2368bcde4f5d13490ce33fad7e3973
alwan <94159444+alwan9@users.noreply.github.com>
2022-12-27 14:11:41 +0700

    Add files via upload

---
+++ web_desain/script.js
@@ -0,0 +1,42 @@
+// darkmode
+function setDarkMode(isDark) {
+  var darkBtn = document.getElementById('darkBtn')
+  var lightBtn = document.getElementById('lightBtn')
+
+
+  document.body.classList.toggle("darkmode");
+}
+
+// navbar
+
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+
+
+//read more
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+
+
+// animasi header
