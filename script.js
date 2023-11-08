d5274a85e5c1e17f67ae4705deeba0235a15e0c6
alwan <94159444+alwan9@users.noreply.github.com>
2023-11-08 13:38:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// darkmode
-function setDarkMode(isDark) {
-  let darkBtn = document.getElementById('darkBtn')
-  document.body.classList.toggle("darkmode");
-}
+
 
 // navbar
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
@@ -13,13 +9,19 @@ toggleButton.addEventListener("click", () => {
 });
 
 
-//read more
-function myFunction() {
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+
+function read1() {
   let dots = document.getElementById("dots");
   let moreText = document.getElementById("more");
   let btnText = document.getElementById("myBtn");
 
-  if (dots.style.display === "none") {
+  if (dots.style.display === "none" || dots.style.display === "") {
     dots.style.display = "inline";
     btnText.innerHTML = "Read more";
     moreText.style.display = "none";
@@ -31,3 +33,132 @@ function myFunction() {
 }
 
 
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+
+
+function PT1() {
+  let dots1 = document.getElementById("dots1");
+  let moreText1 = document.getElementById("more1");
+  let btnText1 = document.getElementById("myBtn1");
+
+  if (dots1.style.display === "none" || dots1.style.display === "") {
+    dots1.style.display = "inline";
+    btnText1.innerHTML = "PT.JAVA MULTI MANDIRI (JVM) ˅";
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none";
+    btnText1.innerHTML = "PT.JAVA MULTI MANDIRI (JVM) ˃";
+    moreText1.style.display = "inline";
+  }
+}
+
+function PT2() {
+  let dots2 = document.getElementById("dots2");
+  let moreText2 = document.getElementById("more2");
+  let btnText2 = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none" || dots2.style.display === "") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "PT.Charisma Cahaya Mulia (PT Arya) ˅ ";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "PT.Charisma Cahaya Mulia (PT Arya) ˃ ";
+    moreText2.style.display = "inline";
+  }
+}
+
+function PT3() {
+  let dots3 = document.getElementById("dots3");
+  let moreText3 = document.getElementById("more3");
+  let btnText3 = document.getElementById("myBtn3");
+
+  if (dots3.style.display === "none" || dots3.style.display === "") {
+    dots3.style.display = "inline";
+    btnText3.innerHTML = "FREELANCE ˅ ";
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none";
+    btnText3.innerHTML = "FREELANCE ˃ ";
+    moreText3.style.display = "inline";
+  }
+}
+
+
+
+
+
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+
+function sekolah1() {
+  let Sdots1 = document.getElementById("Sdots1");
+  let SmoreText1 = document.getElementById("Smore1");
+  let SbtnText1 = document.getElementById("SmyBtn1");
+
+  if (Sdots1.style.display === "none" || Sdots1.style.display === "") {
+    Sdots1.style.display = "inline";
+    SbtnText1.innerHTML = " SMP TELKOM PURWOKERTO ˅";
+    SmoreText1.style.display = "none";
+  } else {
+    Sdots1.style.display = "none";
+    SbtnText1.innerHTML = " SMP TELKOM PURWOKERTO ˃";
+    SmoreText1.style.display = "inline";
+  }
+}
+function sekolah2() {
+  let Sdots2 = document.getElementById("Sdots2");
+  let SmoreText2 = document.getElementById("Smore2");
+  let SbtnText2 = document.getElementById("SmyBtn2");
+
+  if (Sdots2.style.display === "none" || Sdots2.style.display === "") {
+    Sdots2.style.display = "inline";
+    SbtnText2.innerHTML = " SMKN 1 PURWOKERTO ˅";
+    SmoreText2.style.display = "none";
+  } else {
+    Sdots2.style.display = "none";
+    SbtnText2.innerHTML = " SMKN 1 PURWOKERTO ˃";
+    SmoreText2.style.display = "inline";
+  }
+}
+
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================
+// ==========================================================================// Ambil elemen-elemen dengan atribut data-lang
+function changeLanguage() {
+  var languageToggle = document.getElementById("languageToggle");
+  var currentLanguage = languageToggle.innerHTML;
+
+  var contentElements = document.querySelectorAll('.content');
+  if (currentLanguage === "Indonesia") {
+      languageToggle.innerHTML = "English";
+      contentElements.forEach(function (element) {
+          if (element.getAttribute('data-lang') === 'id') {
+              element.style.display = 'none';
+          } else if (element.getAttribute('data-lang') === 'en') {
+              element.style.display = 'block';
+          }
+      });
+  } else {
+      languageToggle.innerHTML = "Indonesia";
+      contentElements.forEach(function (element) {
+          if (element.getAttribute('data-lang') === 'id') {
+              element.style.display = 'block';
+          } else if (element.getAttribute('data-lang') === 'en') {
+              element.style.display = 'none';
+          }
+      });
+  }
+}
